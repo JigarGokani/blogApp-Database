98bc1cfc96b93d807cc1cea60faf789485f8673b
Jigar-gokani <jigar.gokani@somaiya.edu>
2023-09-04 10:42:34 +0530

    postcontroller created

--- routes/blog.js
+++ routes/blog.js
@@ -3,13 +3,18 @@ const router = express.Router();
 
 
 // Import Controller
-const{ dummyLink } = require('../controllers/likeController');
+const{ dummyLink,likePost, unlikePost } = require('../controllers/likeController');
 const{createComment} = require("../controllers/commentController")
+const{createPost,getAllPosts} = require("../controllers/postController");
 
 
 // Mapping Create
 router.get("/dummyroute",dummyLink);
 router.post("/comments/create",createComment);
+router.post("/posts/create",createPost);
+router.get("/posts",getAllPosts);
+router.post("/likes/like",likePost);
+router.post("/likes/unlike",unlikePost);
 
 
 
